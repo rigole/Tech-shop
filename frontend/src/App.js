77947b88558bc55a53c723274cf06cc93524197d
rigole <foplacide@gmail.com>
2022-04-05 18:40:16 +0100

    setting the NavBar 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import './App.css';
@@ -6,8 +7,10 @@ function App() {
   return (
     <div className="App">
         <Header/>
-            <main>
-                <h1>Welcome</h1>
+            <main className="py-3">
+                <Container>
+                    <h1>Welcome</h1>
+                 </Container>
             </main>
         <Footer/>
     </div>
