0ca71db4de64d4d3a820322152b9aaf373d569ea
rigole <foplacide@gmail.com>
2022-04-05 18:52:01 +0100

    setting the NavBar 2

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,14 +4,14 @@ import {Navbar, Nav, Container, Row } from 'react-bootstrap';
 function Header() {
     return (
         <header>
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="light" variant="light" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="/">Tech-Shop</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav className="me-auto" >
+                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                            <Nav.Link href="/login"><i className="fas fa-user"></i>Login</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
