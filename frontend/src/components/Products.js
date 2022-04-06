5c35b6327e73af7631d9a16ba0b089bd833228e4
rigole <foplacide@gmail.com>
2022-04-06 14:06:16 +0100

    creating components products and HomePage and render in the main page of the App

---
+++ frontend/src/components/Products.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function Products() {
+
+    return (
+        <Card className="my-3 p-3 rounded">
+            Products
+        </Card>
+    )
+}
+
+export default Products
